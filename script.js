606f85d540b170acfaab6d2f6ffb021f00f66d3d
fonsy91 <alfonso91fp@gmail.com>
2025-11-28 12:36:09 +0100

    avances pruebas CSS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+
+// Para JUSTIFY CONTENT
+function setJustify(value) {
+    const flexContainer = document.getElementById('flexContainer');
+    if (flexContainer) {
+        flexContainer.style.justifyContent = value;
+    }
+}
+
+// Función global que cambia align-items del contenedor
+function setAlign(value) {
+    const flexContainerItems = document.getElementById('flexContainerItems');
+    if (flexContainerItems) {
+        flexContainerItems.style.alignItems = value;
+    }
+}
